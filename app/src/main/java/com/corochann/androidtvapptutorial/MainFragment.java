13b6fcb997759b4eaf82e627ad5cb1df74ce4a01
unknown <5004121605@MYKL1D000012986.ap.sony.com>
2015-06-29 16:37:35 +0800

    Add Header & Row (GridItemPresenter)

--- app/src/main/java/com/corochann/androidtvapptutorial/MainFragment.java
+++ app/src/main/java/com/corochann/androidtvapptutorial/MainFragment.java
@@ -1,8 +1,17 @@
 package com.corochann.androidtvapptutorial;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v17.leanback.app.BrowseFragment;
+import android.support.v17.leanback.widget.ArrayObjectAdapter;
+import android.support.v17.leanback.widget.HeaderItem;
+import android.support.v17.leanback.widget.ListRow;
+import android.support.v17.leanback.widget.ListRowPresenter;
+import android.support.v17.leanback.widget.Presenter;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by corochann on 2015/06/28.
@@ -10,12 +19,18 @@ import android.util.Log;
 public class MainFragment extends BrowseFragment {
     private static final String TAG = MainFragment.class.getSimpleName();
 
+    private ArrayObjectAdapter mRowsAdapter;
+    private static final int GRID_ITEM_WIDTH = 300;
+    private static final int GRID_ITEM_HEIGHT = 200;
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         Log.i(TAG, "onActivityCreated");
         super.onActivityCreated(savedInstanceState);
 
         setupUIElements();
+
+        loadRows();
     }
 
     private void setupUIElements() {
@@ -30,5 +45,47 @@ public class MainFragment extends BrowseFragment {
         // set search icon color
         setSearchAffordanceColor(getResources().getColor(R.color.search_opaque));
     }
+
+    private void loadRows() {
+        mRowsAdapter = new ArrayObjectAdapter(new ListRowPresenter());
+
+        /* GridItemPresenter */
+        HeaderItem gridItemPresenterHeader = new HeaderItem(0, "GridItemPresenter");
+
+        GridItemPresenter mGridPresenter = new GridItemPresenter();
+        ArrayObjectAdapter gridRowAdapter = new ArrayObjectAdapter(mGridPresenter);
+        gridRowAdapter.add("ITEM 1");
+        gridRowAdapter.add("ITEM 2");
+        gridRowAdapter.add("ITEM 3");
+        mRowsAdapter.add(new ListRow(gridItemPresenterHeader, gridRowAdapter));
+
+        /* set */
+        setAdapter(mRowsAdapter);
+    }
+
+    private class GridItemPresenter extends Presenter {
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent) {
+            TextView view = new TextView(parent.getContext());
+            view.setLayoutParams(new ViewGroup.LayoutParams(GRID_ITEM_WIDTH, GRID_ITEM_HEIGHT));
+            view.setFocusable(true);
+            view.setFocusableInTouchMode(true);
+            view.setBackgroundColor(getResources().getColor(R.color.default_background));
+            view.setTextColor(Color.WHITE);
+            view.setGravity(Gravity.CENTER);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, Object item) {
+            ((TextView) viewHolder.view).setText((String) item);
+        }
+
+        @Override
+        public void onUnbindViewHolder(ViewHolder viewHolder) {
+
+        }
+    }
+
 
 }
