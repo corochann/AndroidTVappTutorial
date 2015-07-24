59a7c63957040af921766bf9599362fdbef2bb06
unknown <5004121605@MYKL1D000012986.ap.sony.com>
2015-07-24 12:00:16 +0800

    GuidedStepActivity & GuidedStepFragment minimum implementation

---
+++ app/src/main/java/com/corochann/androidtvapptutorial/GuidedStepActivity.java
@@ -0,0 +1,84 @@
+package com.corochann.androidtvapptutorial;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v17.leanback.app.GuidedStepFragment;
+import android.support.v17.leanback.widget.GuidanceStylist;
+import android.support.v17.leanback.widget.GuidedAction;
+import android.util.Log;
+
+import java.util.List;
+
+/**
+ * Created by corochann on 24/7/2015.
+ */
+public class GuidedStepActivity extends Activity {
+
+    private static final String TAG = GuidedStepActivity.class.getSimpleName();
+
+    /* Action ID definition */
+    private static final int ACTION_CONTINUE = 0;
+    private static final int ACTION_BACK = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+        if (null == savedInstanceState) {
+            GuidedStepFragment.add(getFragmentManager(), new FirstStepFragment());
+        }
+    }
+
+    private static void addAction(List<GuidedAction> actions, long id, String title, String desc) {
+        actions.add(new GuidedAction.Builder()
+                .id(id)
+                .title(title)
+                .description(desc)
+                .build());
+    }
+
+    public static class FirstStepFragment extends GuidedStepFragment {
+        @NonNull
+        @Override
+        public GuidanceStylist.Guidance onCreateGuidance(Bundle savedInstanceState) {
+            String title = "Title";
+            String breadcrumb = "Breadcrumb";
+            String description = "Description";
+            Drawable icon = getActivity().getDrawable(R.drawable.ic_main_icon);
+
+            return new GuidanceStylist.Guidance(title, description, breadcrumb, icon);
+        }
+
+        @Override
+        public void onCreateActions(@NonNull List<GuidedAction> actions, Bundle savedInstanceState) {
+            addAction(actions, ACTION_CONTINUE, "Continue", "Go to SecondStepFragment");
+            addAction(actions, ACTION_BACK, "Cancel", "Go back");
+        }
+
+        @Override
+        public void onGuidedActionClicked(GuidedAction action) {
+            FragmentManager fm = getFragmentManager();
+            switch ((int) action.getId()){
+                case ACTION_CONTINUE:
+                    // GuidedStepFragment.add(fm, new SecondStepFragment());
+                    break;
+                case ACTION_BACK:
+                    getActivity().finish();
+                    break;
+                default:
+                    Log.w(TAG, "Action is not defined");
+                    break;
+            }
+            if (action.getId() == ACTION_CONTINUE) {
+
+            } else if (action.getId() == ACTION_BACK) {
+                getActivity().finish();
+            } else {
+                Log.w(TAG, "Action is not defined");
+            }
+        }
+    }
+}
--- app/src/main/java/com/corochann/androidtvapptutorial/MainFragment.java
+++ app/src/main/java/com/corochann/androidtvapptutorial/MainFragment.java
@@ -61,9 +61,10 @@ public class MainFragment extends BrowseFragment {
         public void onItemClicked(Presenter.ViewHolder itemViewHolder, Object item,
                                   RowPresenter.ViewHolder rowViewHolder, Row row) {
             // each time the item is clicked, code inside here will be executed.
+            Log.d(TAG, "onItemClicked: item = " + item.toString());
             if (item instanceof Movie) {
                 Movie movie = (Movie) item;
-                Log.d(TAG, "Item: " + item.toString());
+
                 Intent intent = new Intent(getActivity(), DetailsActivity.class);
                 intent.putExtra(DetailsActivity.MOVIE, movie);
 
@@ -71,6 +72,9 @@ public class MainFragment extends BrowseFragment {
             } else if (item instanceof String){
                 if (item == "ErrorFragment") {
                     Intent intent = new Intent(getActivity(), ErrorActivity.class);
+                    startActivity(intent);
+                } else if (item == "GuidedStepFragment") {
+                    Intent intent = new Intent(getActivity(), GuidedStepActivity.class);
                     startActivity(intent);
                 }
             }
@@ -112,7 +116,7 @@ public class MainFragment extends BrowseFragment {
         GridItemPresenter mGridPresenter = new GridItemPresenter();
         ArrayObjectAdapter gridRowAdapter = new ArrayObjectAdapter(mGridPresenter);
         gridRowAdapter.add("ErrorFragment");
-        gridRowAdapter.add("ITEM 2");
+        gridRowAdapter.add("GuidedStepFragment");
         gridRowAdapter.add("ITEM 3");
         mRowsAdapter.add(new ListRow(gridItemPresenterHeader, gridRowAdapter));
 
@@ -121,40 +125,6 @@ public class MainFragment extends BrowseFragment {
         CardPresenter cardPresenter = new CardPresenter();
         ArrayObjectAdapter cardRowAdapter = new ArrayObjectAdapter(cardPresenter);
 
-/*
-        for(int i=0; i<10; i++) {
-            Movie movie = new Movie();
-            if(i%3 == 0) {
-                movie.setCardImageUrl("http://heimkehrend.raindrop.jp/kl-hacker/wp-content/uploads/2014/08/DSC02580.jpg");
-            } else if (i%3 == 1) {
-                movie.setCardImageUrl("http://heimkehrend.raindrop.jp/kl-hacker/wp-content/uploads/2014/08/DSC02630.jpg");
-            } else {
-                movie.setCardImageUrl("http://heimkehrend.raindrop.jp/kl-hacker/wp-content/uploads/2014/08/DSC02529.jpg");
-            }
-            movie.setTitle("title" + i);
-            movie.setStudio("studio" + i);
-            String description = "Lorem ipsum dolor sit amet, qui mundi vivendum cu. Mazim dicant possit te his. Quo solet dicant prodesset eu, pri deseruisse concludaturque ea, saepe maiorum sea et. Impetus discere sed at. Vim eu novum erant integre, te tale voluptatibus est. Facer labores te mel.\n" +
-                    "\n" +
-                    "Dictas denique qualisque mea id, cu mei verear fabellas. Mel no autem nusquam, viderer oblique te mei. At minimum corpora consulatu vim. Cibo nominavi vis no, in verterem vulputate eos, essent iriure cu vel. Ius ferri expetendis ad, omnes aeterno nominati id his, eum debitis lobortis comprehensam id.\n" +
-                    "\n" +
-                    "Illud dicit nostrud sit no. Eu quod nostro pro. Ut gubergren mnesarchum has, nostro detracto scriptorem et quo, no illud phaedrum recteque sea. Ad his summo probatus recusabo. Qui amet tale viris et, ei his quodsi torquatos adipiscing. Laudem malorum no eum, accusam mandamus sit ex, est ut tractatos dissentiet. Dictas feugiat usu et, an his cibo appareat placerat, eu quis dignissim qui.\n" +
-                    "\n" +
-                    "Euripidis neglegentur eu per, denique singulis vel cu, malis dolore ne duo. Cum no iracundia persecuti expetendis. Vim alii dolore malorum at, veniam perfecto salutandi cu nec, vix ad nonumes consulatu scripserit. At sit nonumy dolores aliquando, eu nam sumo legere. Eu maiorum adipisci torquatos his, vidit appareat eos no.\n" +
-                    "\n" +
-                    "Solet laboramus no quo, cu aperiam inermis vix. Eum animal graecis id, ne quodsi abhorreant sit. Tale persequeris te qui. Labitur invenire explicari in vix."
-                    + "Lorem ipsum dolor sit amet, qui mundi vivendum cu. Mazim dicant possit te his. Quo solet dicant prodesset eu, pri deseruisse concludaturque ea, saepe maiorum sea et. Impetus discere sed at. Vim eu novum erant integre, te tale voluptatibus est. Facer labores te mel.\n" +
-                    "\n" +
-                    "Dictas denique qualisque mea id, cu mei verear fabellas. Mel no autem nusquam, viderer oblique te mei. At minimum corpora consulatu vim. Cibo nominavi vis no, in verterem vulputate eos, essent iriure cu vel. Ius ferri expetendis ad, omnes aeterno nominati id his, eum debitis lobortis comprehensam id.\n" +
-                    "\n" +
-                    "Illud dicit nostrud sit no. Eu quod nostro pro. Ut gubergren mnesarchum has, nostro detracto scriptorem et quo, no illud phaedrum recteque sea. Ad his summo probatus recusabo. Qui amet tale viris et, ei his quodsi torquatos adipiscing. Laudem malorum no eum, accusam mandamus sit ex, est ut tractatos dissentiet. Dictas feugiat usu et, an his cibo appareat placerat, eu quis dignissim qui.\n" +
-                    "\n" +
-                    "Euripidis neglegentur eu per, denique singulis vel cu, malis dolore ne duo. Cum no iracundia persecuti expetendis. Vim alii dolore malorum at, veniam perfecto salutandi cu nec, vix ad nonumes consulatu scripserit. At sit nonumy dolores aliquando, eu nam sumo legere. Eu maiorum adipisci torquatos his, vidit appareat eos no.\n" +
-                    "\n" +
-                    "Solet laboramus no quo, cu aperiam inermis vix. Eum animal graecis id, ne quodsi abhorreant sit. Tale persequeris te qui. Labitur invenire explicari in vix.";
-            movie.setDescription(description);
-            cardRowAdapter.add(movie);
-        }
-*/
         ArrayList<Movie> mItems = MovieProvider.getMovieItems();
         for (Movie movie : mItems) {
             cardRowAdapter.add(movie);
