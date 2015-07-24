e0d4d6a035c0ea4d11da29cf3f39ec57f886ecf1
unknown <5004121605@MYKL1D000012986.ap.sony.com>
2015-07-24 18:05:59 +0800

    SecondStepFragment

--- app/src/main/java/com/corochann/androidtvapptutorial/GuidedStepActivity.java
+++ app/src/main/java/com/corochann/androidtvapptutorial/GuidedStepActivity.java
@@ -2,6 +2,7 @@ package com.corochann.androidtvapptutorial;
 
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,6 +10,7 @@ import android.support.v17.leanback.app.GuidedStepFragment;
 import android.support.v17.leanback.widget.GuidanceStylist;
 import android.support.v17.leanback.widget.GuidedAction;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -22,6 +24,15 @@ public class GuidedStepActivity extends Activity {
     /* Action ID definition */
     private static final int ACTION_CONTINUE = 0;
     private static final int ACTION_BACK = 1;
+    private static final int OPTION_CHECK_SET_ID = 10;
+
+    private static final String[] OPTION_NAMES = {"Option A", "Option B", "Option C"};
+    private static final String[] OPTION_DESCRIPTIONS = {"Here's one thing you can do",
+            "Here's another thing you can do", "Here's one more thing you can do"};
+    private static final int[] OPTION_DRAWABLES = {R.drawable.ic_guidedstep_option_a,
+            R.drawable.ic_guidedstep_option_b, R.drawable.ic_guidedstep_option_c};
+    private static final boolean[] OPTION_CHECKED = {true, false, false};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +51,24 @@ public class GuidedStepActivity extends Activity {
                 .build());
     }
 
+    private static void addCheckedAction(List<GuidedAction> actions, int iconResId, Context context,
+                                         String title, String desc, boolean checked) {
+        GuidedAction guidedAction = new GuidedAction.Builder()
+                .title(title)
+                .description(desc)
+                .checkSetId(OPTION_CHECK_SET_ID)
+                .iconResourceId(iconResId, context)
+                .build();
+        guidedAction.setChecked(checked);
+        actions.add(guidedAction);
+    }
+
     public static class FirstStepFragment extends GuidedStepFragment {
+        @Override
+        public int onProvideTheme() {
+            return R.style.Theme_Example_Leanback_GuidedStep_First;
+        }
+
         @NonNull
         @Override
         public GuidanceStylist.Guidance onCreateGuidance(Bundle savedInstanceState) {
@@ -63,8 +91,8 @@ public class GuidedStepActivity extends Activity {
 
             switch ((int) action.getId()){
                 case ACTION_CONTINUE:
-                    // FragmentManager fm = getFragmentManager();
-                    // GuidedStepFragment.add(fm, new SecondStepFragment());
+                    FragmentManager fm = getFragmentManager();
+                    GuidedStepFragment.add(fm, new SecondStepFragment());
                     break;
                 case ACTION_BACK:
                     getActivity().finish();
@@ -73,6 +101,57 @@ public class GuidedStepActivity extends Activity {
                     Log.w(TAG, "Action is not defined");
                     break;
             }
+        }
+    }
+
+    public static class SecondStepFragment extends GuidedStepFragment {
+        @NonNull
+        @Override
+        public GuidanceStylist.Guidance onCreateGuidance(Bundle savedInstanceState) {
+            String title = "SecondStepFragment";
+            String breadcrumb = "Guided Steps: 2";
+            String description ="Showcasing different action configurations";
+            Drawable icon = getActivity().getDrawable(R.drawable.ic_main_icon);
+            return new GuidanceStylist.Guidance(title, description, breadcrumb, icon);
+        }
+
+        @Override
+        public void onCreateActions(List<GuidedAction> actions, Bundle savedInstanceState) {
+            String title = "infoOnly action";
+            String desc = "infoOnly indicates whether this action is for information purposes only and cannot be clicked.\n" +
+                    "The description can be long, by set multilineDescription to true";
+
+            actions.add(new GuidedAction.Builder()
+                    .title(title)
+                    .description(desc)
+                    .multilineDescription(true)
+                    .infoOnly(true)
+                    .enabled(false)
+                    .build());
+            for (int i = 0; i < OPTION_NAMES.length; i++) {
+                addCheckedAction(actions,
+                        OPTION_DRAWABLES[i],
+                        getActivity(),
+                        OPTION_NAMES[i],
+                        OPTION_DESCRIPTIONS[i],
+                        OPTION_CHECKED[i]);
+            }
+        }
+
+        @Override
+        public void onGuidedActionClicked(GuidedAction action) {
+            String text = OPTION_NAMES[getSelectedActionPosition() - 1] + " clicked";
+            Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public GuidanceStylist onCreateGuidanceStylist() {
+            return new GuidanceStylist() {
+                @Override
+                public int onProvideLayoutId() {
+                    return R.layout.guidedstep_second_guidance;
+                }
+            };
         }
     }
 }
