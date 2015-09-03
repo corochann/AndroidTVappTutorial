30c9140dafd88e66e74b353f8587c2121d5975b2
unknown <5004121605@MYKL1D000012986.ap.sony.com>
2015-09-03 20:43:08 +0800

    Search algorithm implementation

--- app/src/main/java/com/corochann/androidtvapptutorial/SearchFragment.java
+++ app/src/main/java/com/corochann/androidtvapptutorial/SearchFragment.java
@@ -4,27 +4,35 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v17.leanback.widget.ArrayObjectAdapter;
 import android.support.v17.leanback.widget.HeaderItem;
 import android.support.v17.leanback.widget.ListRow;
 import android.support.v17.leanback.widget.ListRowPresenter;
 import android.support.v17.leanback.widget.ObjectAdapter;
 import android.support.v17.leanback.widget.SpeechRecognitionCallback;
+import android.text.TextUtils;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
-/**
- * Created by corochann on 2/9/2015.
- */
 public class SearchFragment extends android.support.v17.leanback.app.SearchFragment
         implements android.support.v17.leanback.app.SearchFragment.SearchResultProvider {
 
     private static final String TAG = SearchFragment.class.getSimpleName();
 
     private static final int REQUEST_SPEECH = 0x00000010;
+
     private ArrayObjectAdapter mRowsAdapter;
+    private ArrayList<Movie> mItems = MovieProvider.getMovieItems();
+
+    private String mQuery;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -75,30 +83,59 @@ public class SearchFragment extends android.support.v17.leanback.app.SearchFragm
     @Override
     public ObjectAdapter getResultsAdapter() {
         Log.d(TAG, "getResultsAdapter");
-        Log.d(TAG, mRowsAdapter.toString());
-
-        // It should return search result here,
-        // but static Movie Item list will be returned here now for practice.
-        ArrayList<Movie> mItems = MovieProvider.getMovieItems();
-        ArrayObjectAdapter listRowAdapter = new ArrayObjectAdapter(new CardPresenter());
-        listRowAdapter.addAll(0, mItems);
-        HeaderItem header = new HeaderItem("Search results");
-        mRowsAdapter.add(new ListRow(header, listRowAdapter));
-
+        // Delete previously implemented mock code.
+        // mRowsAdapter (Search result) is already prepared in loadRows method
         return mRowsAdapter;
     }
 
     @Override
     public boolean onQueryTextChange(String newQuery){
         Log.i(TAG, String.format("Search Query Text Change %s", newQuery));
-        //loadQuery(newQuery);
         return true;
     }
 
     @Override
     public boolean onQueryTextSubmit(String query) {
         Log.i(TAG, String.format("Search Query Text Submit %s", query));
-        //loadQuery(query);
+        mQuery = query;
+        loadRows();
         return true;
+    }
+
+    private void loadRows() {
+        // offload processing from the UI thread
+        new AsyncTask<String, Void, ListRow>() {
+            private final String query = mQuery;
+
+            @Override
+            protected void onPreExecute() {
+                mRowsAdapter.clear();
+            }
+
+            @Override
+            protected ListRow doInBackground(String... params) {
+                final List<Movie> result = new ArrayList<>();
+                for (Movie movie : mItems) {
+                    // Main logic of search is here.
+                    // Just check that "query" is contained in Title or Description or not. (NOTE: excluded studio information here)
+                    if (movie.getTitle().toLowerCase(Locale.ENGLISH)
+                            .contains(query.toLowerCase(Locale.ENGLISH))
+                            || movie.getDescription().toLowerCase(Locale.ENGLISH)
+                            .contains(query.toLowerCase(Locale.ENGLISH))) {
+                        result.add(movie);
+                    }
+                }
+
+                ArrayObjectAdapter listRowAdapter = new ArrayObjectAdapter(new CardPresenter());
+                listRowAdapter.addAll(0, result);
+                HeaderItem header = new HeaderItem("Search Results");
+                return new ListRow(header, listRowAdapter);
+            }
+
+            @Override
+            protected void onPostExecute(ListRow listRow) {
+                mRowsAdapter.add(listRow);
+            }
+        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 }
